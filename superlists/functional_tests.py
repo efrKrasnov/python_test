4d62d8f3bac9ce8d596f28f2838941b1fa8669a6
Vitaliy Krasnov <efrKras@gmail.com>
2021-11-15 21:55:56 +0300

    Front page HTML now generated from a template

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -30,12 +30,14 @@ class NewVisitorTest(unittest.TestCase):
 
         inputbox.send_keys('Buy peacock feathers')
 
-        input.send_keys(Keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table: WebElement = self.browser.find_element_by_id('id_list_table')
         rows: List[WebElement] = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows))
+        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
         
         self.fail('Finish the test!')
 
